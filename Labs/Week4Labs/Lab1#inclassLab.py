84953144c0da895035828fba04c6e9a6210c5df2
bobthefish29 <dapunchak@email.neit.edu>
2024-01-31 11:29:56 -0500

    W4InClassLabDone

--- Labs/Week4Labs/Lab1#inclassLab.py
+++ Labs/Week4Labs/Lab1#inclassLab.py
@@ -1,10 +1,13 @@
 #David punchak
-#SE116 int-mid python
+#SE126 int-mid python
 #1/30/24
+#LAB#4 In class lab
 
 
 
 #This is like the demo we did in class but we need to have a few different changes
+
+#I combined the outputs that have the letter grade and the average for the student. It was going to look wierd if it was just the average than one with just the student letter grade
 
 #The final outputs need to be a basic list, a list with the average number of scores and the letter grade that goes with it
 
@@ -17,14 +20,10 @@
 
 ###############################################################
 
-
-
-
-
 import csv
 
 #making lists here
-#This is a 1 d list that just have []
+#This is a 1 d lists
 
 fName = []
 lName = []
@@ -34,7 +33,7 @@ test3 = []
 
 #opping the file
 
-with open("SE126/Week4/TextFile\listPractice1-1.txt") as csvFile:
+with open("Week4/TextFile/listPractice1-1.txt") as csvFile:
 
     file = csv.reader(csvFile)
 
@@ -54,7 +53,7 @@ print("First name \t Last Name \t 1st Test \t 2nt test\t 3rd test\t")
 print("-----------------------------------------------------------------------------")
 #this is a loop for display
 for i in range(0, len(fName)):
-    print(f"{lName[i]:12} \t {fName[i]:12}  \t {test1[i]} \t\t {test2[i]} \t\t {test3[i]}")
+    print(f"{fName[i]:12} \t {lName[i]:12}  \t {test1[i]} \t\t {test2[i]} \t\t {test3[i]}")
 print("----------------------------------------------------------------------------")
 
 #this is some of the new vars and lists for the averages
@@ -66,11 +65,20 @@ class_avg = 0
 average = []
 letter_avg = []
 
+print(f"{'FName'} \t\t  {'LName'}  \t {'Average'} \t {'Letter Grade'}")
+print("--------------------------------------------------------------------------------")
 #this is the main for loop that does the math and getting the letter value
 for i in range(0, len(test1)):
 
     #doing the math for the avarage and adding the avg to the total class avg
     avg = (test1[i] + test2[i] + test3[i]) / 3 
+
+
+    #this is the round command, it rounds the numbers to the 2 decnmal point, I had to google it to use it but it loops better.
+    avg = round(avg, 2)
+
+
+
     class_avg = class_avg + avg
 
     #adding the average to the list
@@ -90,13 +98,46 @@ for i in range(0, len(test1)):
 
 
     #just printing random things that might be trashed
-    print(f"{fName[i]} has a {average[i]:.1f} that is a {letter_avg[i]}")
+    print(f"{fName[i]:8} \t {lName[i]:8}  \t  {average[i]:4.1f} \t\t    {letter_avg[i]}")
     total_count += 1
 
 #doing the math for the class total average
 class_avg = class_avg / total_count
 
-print(class_avg)
+#Just to find the letter vale that the class average is, its going to be defined here and just live here.
+classLetterAvg = ""
+
+if class_avg >= 90 and avg <= 100:#if it is 90 - 100 it is an A and so on
+    classLetterAvg = "A"
+elif class_avg >= 80 and avg <= 89:
+    classLetterAvg = "B"
+elif class_avg >= 70 and avg <= 79:
+    classLetterAvg = "C"
+elif class_avg >= 60 and avg <= 69:
+    classLetterAvg = "D"
+else:
+    classLetterAvg = "F"
+
+
+
+print(f"\nThe class average is {class_avg:.1f}, that would be an {classLetterAvg}.")
+
 
 #this is going to the place were i have the displayes for the 2D list
+all_student = []
 
+for i in range(0, len(fName)):
+
+    #adding the student data to there (index) place in the all_students list[]
+    #all_student.append("["+ "'"+str(fName[i])+ "'" + "," + "'"+ str(lName[i])+ "'" + "," + "'"+ str(test1[i])+ "'" + "," + "'"+ str(test2[i])+ "'" + "," + "'"+ str(test3[i])+"'" + "]")#this is def the hardest way to do it, but i did it this way
+
+    all_student.append([fName[i], lName[i], test1[i], test2[i], test3[i], average[i], letter_avg[i] ])
+
+print(f"\n------------------2D-list printing---------------------------\n")
+print("['First Name', 'Last Name', Test1, Test2, Test3, Average, 'letter grade' ]")
+print("-----------------------------------------------------------------------------------")
+for i in range(0, len(all_student)):
+    print(all_student[i])
+
+
+input("\n--------------------------Press Enter To Quit :p----------------------------------")
