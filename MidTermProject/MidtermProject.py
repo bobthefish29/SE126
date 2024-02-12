7f22ba844c378d3f052c8c6f7b85a7ac9515f895
bobthefish29 <dapunchak@email.neit.edu>
2024-02-12 15:39:07 -0500

    LotOfMidterm

--- MidTermProject/MidtermProject.py
+++ MidTermProject/MidtermProject.py
@@ -59,11 +59,13 @@ pokeDefSpeed = []
 pokeSpeed = []
 pokeGen = []
 pokeLeg = []
+pokeAll =[]
+
+playerPokemon = []
 
 #the vars for the projects
 randDeside = 0
 coins = 0
-live = 3
 
 
 #--------------------This is where i am going to be adding the values to there respected list before i use them.
@@ -92,15 +94,17 @@ with open("MidTermProject/TextFile/Pokemon.csv") as pokeFile:
     for rec in pokiefile:
         #adding the rec of the pokemon list to there location
         pokeNum.append(int(rec[0]))
+        
         pokeName.append(rec[1])
+        
         pokeType.append(rec[2])
-
+        
         #This is there so if the pokemon has a type 2 or not
         if rec[3] == "":
             pokeType2.append("NUN")
         else:
             pokeType2.append(str(rec[3]))
-
+        
         
         #readding the values to there lists
         pokeTotal.append(int(rec[4]))
@@ -109,10 +113,18 @@ with open("MidTermProject/TextFile/Pokemon.csv") as pokeFile:
         pokeAttSpeed.append(int(rec[7]))
         pokeDefSpeed.append(int(rec[8]))
         pokeSpeed.append(int(rec[9]))
-        pokeGen.append(int(rec[10]))
-        pokeLeg.append(rec[11])
-#Out of loop
 
+        pokeGen.append(int(rec[11]))
+
+        #print(str(rec[12]))
+        if str(rec[12]) == "False":
+            pokeLeg.append("No")
+        else:
+            pokeLeg.append("Yes")
+    #making the 2d list for the pokiemon
+    for i in range(0, len(pokeNum)):
+        pokeAll.append([pokeNum[i], pokeName[i], pokeType[i], pokeType2[i], pokeTotal[i], pokeAtt[i], pokeDef[i], pokeAttSpeed[i], pokeDefSpeed[i], pokeSpeed[i], pokeGen[i], pokeLeg[i]])
+    
 
 #----------------------------------Functions------------------------------
 #this is where the functions are going ton be for my program
@@ -128,7 +140,6 @@ def clear():
     # for mac and linux(here, os.name is 'posix') 
     else: 
         _ = system('clear')
-
 
 #menu function
 def menu():
@@ -151,15 +162,15 @@ def menu():
     clear()
     return userInput
 
-def longLatGame(hp, money):
+#this is the function to call the long and lat game
+def longLatGame(money):
+
     #The starting for the game
-    round = 10
 
 
     #printing for the user to read
     print("------This game is hard------\n")
-    print(f"\tYour Total hp: {hp}")
-    print(f"\tYou have: {money}")
+    print(f"\tYou have {money} coins")
     gamePicker = input("\nGame 1 more money, more Qustions\nGame 2 less money, less qustions\n\n(1 or 2): ")
     while gamePicker != "1" and gamePicker != "2":
         print("wrong")
@@ -174,130 +185,324 @@ def longLatGame(hp, money):
     time.sleep(3)
     clear()
     
+    #having the local vars for the function
+
     
-    while hp > 0 and round != 0:
         
-        if gamePicker == "1":
-            copyList = []
-            copyList = longLat2d
-            print(copyList)
-            gameList = []
-            rand_num = 0
-            start_len = len(copyList)
-            index = 0
+    if gamePicker == "1":
+        money = moreRoundMoreMoney(money)
+    else:
+        money = lessRoundLessMoney(money)
             
-            while index < start_len:
-                #THis is sorting the list to make it into a random list
-                print(f"\n------1Run time: {index}------------")
-                #print(f"1Copy len:{len(copyList)}")
-                #Setting to a random num based on copy
-                rand_num = random.randint(0, len(copyList))
-                #print(f"1Rand num: {rand_num}")
-                #if its over than it would subtract than set it to that, the -1 is because the it index would set it to 0 instead
-                if rand_num == -1:
-                    rand_num = 0
-                elif rand_num >= len(copyList):
-                    rand_num -= 1
-                    #print("\t------over-------")
-
-                gameList.append(copyList[rand_num])
-                #print(gameList)
-                #print(f"Before{copyList}")
-
-                #print(f"\n2Run time: {index}")
-                #print(f"2Rand num: {rand_num}")
-                copyList.pop(rand_num)
-                #print(f"2Copy len:{len(copyList)}")
-                #print(f"after{copyList}")
-                #print(f"\n3Run time: {index}")
-                #print(f"3Rand num: {rand_num}")
-                index += 1
-
-            print(gameList)
-
-
-            #----------------------------Up to point of trying to get print to work. i have a lot to go :)
-            print("--------------------")
-            #print(ranList)
-
-            
-            randomPlace = random.randint(0, len(gameList))
-            randomPlace1 = random.randint(0, len(gameList))
-            randomPlace2 = random.randint(0, len(gameList))
-            
-            if randomPlace == -1:
-                    randomPlace = 0
-            elif randomPlace >= len(gameList):
-                    randomPlace -= 1
-
-            printlist = []
-
-            printlist2 = []
-            printlist3 = []
-            printlist4 = []
-
-            for i in range(0, len(gameList)):
-                printlist.append(gameList[1][0])
-
-                printlist2.append(gameList[1][1])
-                printlist3.append(gameList[2][1])
-                printlist4.append(gameList[1][0])
-            print(printlist)
-
-
-
-            print(f"What is the location of {printlist[randomPlace]}")
-
-
-            print(f"A. {printlist2[randomPlace]}")
-
-
-
-            #1
-            #print(f"What is the location of {location[ranlonglat]}")
-
-            #ranList.append(long[there], lat[there])
-
-            
-            
-            #print(there)
-            #print(f"\nA. Long {long[ranlonglat]}, Lat: {lat[ranlonglat]}  ")
-
-            #print(f"B. {ranList[there]} ")
-            #print(f"c. {ranList[there]} ")
-
-
-            #print(f"B. {ranList[there]}")
-            #print(f"c. {ranList[there]}")
-            #print(f"D. {ranList[there]}")
-
-            #print(f"C. Long {long[redHar2]}, Lat: {lat[redHar2]}  ")
-            #print(f"D. Long {long[redHar3]}, Lat: {lat[redHar3]}  ")
-
-            longLatGamePick = input("\nWhat is your answer: ")
-
-            while longLatGamePick != "A" and longLatGamePick != "a" and longLatGamePick != "B" and longLatGamePick != "b" and longLatGamePick != "C" and longLatGamePick != "c" and longLatGamePick != "D" and longLatGamePick != "d":
-                print("-------You Have to pick A,B,C,D silly---------")
-                longLatGamePick = input("\nWhat is your answer: ")
-
-
         
-
-
-
-            
-
-            
-
-
-
+    #print("out of loop ")
+    #print(f"Round count {roundCount}")
+    print(f"You were able to get ${money} this time")
+    time.sleep(2)
 
 
     return money
+
+#-------------Sub long lat functions---------------
+#Sub function for the long lat game that would give more money and have more rounds
+def moreRoundMoreMoney(money):
+    runTime = 10
+    gameList = []
+    copyList = []
+    money = money
     
 
+    while runTime != 0: 
+        print(f"\t--{runTime} more qustion('s)--\n")
+        copyList = longLat2d
+        #copyList = longLat2d
+        #copyList.append(longLat2d)
 
-#
+        #print(f"CopyList {copyList}")
+            
+        #gameList = []
+        rand_num = 0
+        start_len = len(longLat2d)
+        index = 0
+        #print(f"Start len: {start_len}")
+        #print(index)
+        while index < start_len:
+            #THis is sorting the list to make it into a random list
+            #print(f"\n------1Run time: {index}------------")
+            #print(f"1Copy len:{len(copyList)}")
+            #Setting to a random num based on copy
+            rand_num = random.randint(0, len(copyList))
+            #print(f"1Rand num: {rand_num}")
+            #if its over than it would subtract than set it to that, the -1 is because the it index would set it to 0 instead
+            if rand_num == -1:
+                rand_num = 0
+            elif rand_num >= len(copyList):
+                rand_num -= 1
+                #print("\t------over-------")
+
+            gameList.append(copyList[rand_num])
+
+            index += 1
+
+
+        randomPlace = random.randint(0, len(gameList))
+        randomPlace1 = random.randint(0, len(gameList))
+        randomPlace2 = random.randint(0, len(gameList))
+        randomPlace3 = random.randint(0, len(gameList))
+    
+        #this is for if the random number is equal or grader than the len of the list it wont brake and say its out of range
+        if randomPlace == -1:
+            randomPlace = 0
+        elif randomPlace >= len(gameList):
+            randomPlace -= 1
+        if randomPlace1 == -1:
+            randomPlace1 = 0
+        elif randomPlace1 >= len(gameList):
+            randomPlace1 -= 1
+        if randomPlace2 == -1:
+            randomPlace2 = 0
+        elif randomPlace2 >= len(gameList):
+            randomPlace2 -= 1
+        if randomPlace3 == -1:
+            randomPlace3 = 0
+        elif randomPlace3 >= len(gameList):
+            randomPlace3 -= 1
+
+
+
+
+        print(f"What is the location of {gameList[randomPlace][0]}")
+
+        #Will alwasy be the same answer of a to get coins
+        print(f"\nA. Lat: {float(gameList[randomPlace][1]):.2f} Long: {float(gameList[randomPlace][2]):.2f}")
+
+        print(f"B. Lat: {float(gameList[randomPlace1][1]):.2f} Long: {float(gameList[randomPlace1][2]):.2f}")
+        print(f"C. Lat: {float(gameList[randomPlace2][1]):.2f} Long: {float(gameList[randomPlace2][2]):.2f}")
+        print(f"D. Lat: {float(gameList[randomPlace3][1]):.2f} Long: {float(gameList[randomPlace3][2]):.2f}")
+
+
+        
+
+        longLatGamePick = input("\nWhat is your answer: ")
+
+        while longLatGamePick != "A" and longLatGamePick != "a" and longLatGamePick != "B" and longLatGamePick != "b" and longLatGamePick != "C" and longLatGamePick != "c" and longLatGamePick != "D" and longLatGamePick != "d":
+            print("-------You Have to pick A,B,C,D silly---------")
+            longLatGamePick = input("\nWhat is your answer: ")
+
+
+        for i in range(0, len(location)):
+            gameList.pop(0)
+
+        #print(f"After gamelist : {gameList}")
+        index = 0
+        copyList = []
+        
+        #if #the long, lat the user picks == the rec location in the list: true
+
+        #this is where i am stuck, 
+        if longLatGamePick == "A" or longLatGamePick == "a":
+            money += 1
+        elif longLatGamePick == "B" or longLatGamePick == "b":
+            money += 0
+        elif longLatGamePick == "C" or longLatGamePick == "c":
+            money += 0
+        else:
+            money += 0
+
+
+
+        runTime -= 1
+        #print(runTime)
+        
+        clear()
+
+    return money
+
+#Sub function for the long, lat game aboue less money less rounds
+def lessRoundLessMoney(money):
+    runTime = 5
+    gameList = []
+    copyList = []
+    money = money
+    
+
+    #print(f"runTime: {runTime}")
+    #copyList = []
+
+    while runTime != 0:  
+        print(f"\t--{runTime} more qustion('s)--\n")
+        copyList = longLat2d
+        #copyList = longLat2d
+        #copyList.append(longLat2d)
+
+        #print(f"CopyList {copyList}")
+            
+        #gameList = []
+        rand_num = 0
+        start_len = len(longLat2d)
+        index = 0
+        #print(f"Start len: {start_len}")
+        #print(index)
+        while index < start_len:
+            #THis is sorting the list to make it into a random list
+            #print(f"\n------1Run time: {index}------------")
+            #print(f"1Copy len:{len(copyList)}")
+            #Setting to a random num based on copy
+            rand_num = random.randint(0, len(copyList))
+            #print(f"1Rand num: {rand_num}")
+            #if its over than it would subtract than set it to that, the -1 is because the it index would set it to 0 instead
+            if rand_num == -1:
+                rand_num = 0
+            elif rand_num >= len(copyList):
+                rand_num -= 1
+                #print("\t------over-------")
+
+            gameList.append(copyList[rand_num])
+
+            index += 1
+
+
+        randomPlace = random.randint(0, len(gameList))
+        randomPlace1 = random.randint(0, len(gameList))
+        randomPlace2 = random.randint(0, len(gameList))
+        randomPlace3 = random.randint(0, len(gameList))
+    
+        #this is for if the random number is equal or grader than the len of the list it wont brake and say its out of range
+        if randomPlace == -1:
+            randomPlace = 0
+        elif randomPlace >= len(gameList):
+            randomPlace -= 1
+        if randomPlace1 == -1:
+            randomPlace1 = 0
+        elif randomPlace1 >= len(gameList):
+            randomPlace1 -= 1
+        if randomPlace2 == -1:
+            randomPlace2 = 0
+        elif randomPlace2 >= len(gameList):
+            randomPlace2 -= 1
+        if randomPlace3 == -1:
+            randomPlace3 = 0
+        elif randomPlace3 >= len(gameList):
+            randomPlace3 -= 1
+
+
+
+
+        print(f"What is the location of {gameList[randomPlace][0]}")
+
+
+        print(f"\nA. Lat: {float(gameList[randomPlace][1]):.2f} Long: {float(gameList[randomPlace][2]):.2f}")
+
+        print(f"B. Lat: {float(gameList[randomPlace1][1]):.2f} Long: {float(gameList[randomPlace1][2]):.2f}")
+        print(f"C. Lat: {float(gameList[randomPlace2][1]):.2f} Long: {float(gameList[randomPlace2][2]):.2f}")
+        print(f"D. Lat: {float(gameList[randomPlace3][1]):.2f} Long: {float(gameList[randomPlace3][2]):.2f}")
+
+
+
+        longLatGamePick = input("\nWhat is your answer: ")
+
+        while longLatGamePick != "A" and longLatGamePick != "a" and longLatGamePick != "B" and longLatGamePick != "b" and longLatGamePick != "C" and longLatGamePick != "c" and longLatGamePick != "D" and longLatGamePick != "d":
+            print("-------You Have to pick A,B,C,D silly---------")
+            longLatGamePick = input("\nWhat is your answer: ")
+
+
+        for i in range(0, len(location)):
+            gameList.pop(0)
+
+        #print(f"After gamelist : {gameList}")
+        index = 0
+        copyList = []
+        
+        #if #the long, lat the user picks == the rec location in the list: true
+
+        #this is where i am stuck, 
+        if longLatGamePick == "A" or longLatGamePick == "a":
+            money += 1
+        elif longLatGamePick == "B" or longLatGamePick == "b":
+            money += 0
+        elif longLatGamePick == "C" or longLatGamePick == "c":
+            money += 0
+        else:
+            money += 0
+
+
+
+        runTime -= 1
+        #print(runTime)
+
+    return money
+
+#------------Start pokemon functions-------------
+#this is the function to call the pokemon game
+def pokeMonGame(cash):
+    money = cash
+    pokiemon = []
+
+    print("\t\t------Welcome User To the Slots-----\n")
+
+
+
+
+    userPick = input("\t\t-0 is to quit-\n What Slot would you like to do user(1-3)?: ")
+
+
+    while userPick != "1" and userPick != "2" and userPick != "3" and userPick != "0":
+        userPick = input("\t---Silly user you have to enter '1-3' an 0 to quit: ")
+
+
+
+    while userPick != "0":
+        if userPick == "1" and money >= 5:
+            prize = pokeGame1All()
+            pokiemon.append(prize)
+            money -= 5
+        elif userPick == "2" and money >= 10:
+            print("2")
+            money -= 10
+        elif userPick == "3" and money >= 20:
+            print("3")
+            money -= 20
+        else:
+            print("\t\t------Broke boy--------")
+            
+            
+
+
+
+
+
+        userPick = input("\t\t-0 is to quit-\n What Slot would you like to do user?: ")
+
+        while userPick != "1" and userPick != "2" and userPick != "3" and userPick != "0":
+            userPick = input("\t---Silly user you have to enter '1-3' an 0 to quit: ")
+
+    return money, pokiemon
+        
+#----------Sub pokemon-----------------
+#sub pokemon slot games: this is the different slot functions only have the 1 slot now
+def pokeGame1All():
+    allPokemonran = random.randint(0, len(pokeAll))
+
+    if allPokemonran > len(pokeNum):
+        print("\n\t\t-------PokemonRan number over---------")
+        allPokemonran -= 1
+    clear()#clear the screen
+    print("----3----")#the count down 3
+    time.sleep(1)
+    print("---2---") #2
+    time.sleep(1)
+    print("--1--")# 1
+    time.sleep(1)
+
+    #print(pokeAll[721][11])
+    print(f"\t\t----You got #{pokeAll[allPokemonran][0]}: {pokeAll[allPokemonran][1]}----\nThey are a {pokeAll[allPokemonran][2]} and a {pokeAll[allPokemonran][3]} type.\n They are from gen {pokeAll[allPokemonran][10]}.\n\t----Are they a lengendary---\n\t\t{pokeAll[allPokemonran][11]}")# this is the pokiemon the user got, based on the pick they choose.
+    
+
+    wonPrize = pokeAll[allPokemonran]
+    time.sleep(6)
+    clear()
+    return wonPrize
+#Need to have the other 3 for gen and leg.
 
 #----------------------This is where i will have my main code------------------------------------
 
@@ -312,26 +517,30 @@ gamePick = int(menu())
 while gamePick != 4:
 
     if gamePick == 1:
-        coins = longLatGame(live, coins)
-        print("out of long lat")
+        coins = longLatGame(coins)
+        print(f"Total$: {coins}")
     elif gamePick == 2:
-        print(" '2'  ")
+        coins, playerPokemon = pokeMonGame(coins)
+        print(f"Total$: {coins}")
     else:
-        print(" '3'  ")
+        print("-------Still working----------")
 
     gamePick = int(menu())
 
 
-input("----------------------Stop-----------------")
+input("----------------------Enter to Stop-----------------")
 
 
 
 
 
 
+#The long and lat game is done, need to change prints around and clean up but main code there (if i can some how have time at end to fix the qustions than do it if not its okay wont b e much point lost)
 
 
+#Need to work on the pokemon functions for the other slots
 
+#need to work on the finding pokiemon(Just be a display of the pokiemon the user has. --------Nothing specal--------)
 
 
 
